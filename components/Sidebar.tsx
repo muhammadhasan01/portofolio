ade9ef63313a6b4f671cb439d67beb3b63752e5f
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-11 02:52:50 +0700

    feat: add Sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,18 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <img
+        src='https://ik.imagekit.io/2louui6ojn7/portofolio/profile-picture-hasan_P1ExkjSgN?updatedAt=1630422826587'
+        alt='image profile'
+      />
+      <h3>
+        <span>Muhammad</span>
+        Hasan
+      </h3>
+      <p>Final Year Student</p>
+      <p>Download Resume</p>
+    </div>
+  );
+};
+
+export default Sidebar;
