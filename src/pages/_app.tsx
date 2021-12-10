7701899014fdc69ba175eb9d76c316501105eecb
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-11 01:53:09 +0700

    build: rebuild project

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app';
-import 'antd/dist/antd.css';
-import 'tailwindcss/tailwind.css';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import AboutMe from '@/about/AboutMe';
-
-const Home = () => (
-  <div className="container mx-auto">
-    <AboutMe />
-  </div>
-);
-
-export default Home;
