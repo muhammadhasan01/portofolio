c68880995dcb11e78c2eeb9c6fc63f285139b5f7
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-11 02:43:45 +0700

    chore: reformat with prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='grid grid-cols-12 gap-6 my-14 px-48'>
+      <div className='col-span-3 bg-white rounded-2xl'>
+        sidebar
+      </div>
+      <div className='col-span-9 bg-white rounded-2xl'>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
