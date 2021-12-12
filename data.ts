0f283ff3d6c603d4bfa4c863da30a9715bcb14ab
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-12 17:48:12 +0700

    chore: use NextJS props

---
+++ data.ts
@@ -0,0 +1,25 @@
+import { RiComputerLine } from 'react-icons/ri';
+import { IService } from './type';
+
+export const services: IService[] = [
+  {
+    title: 'Title Something 1',
+    about: 'About Something 1',
+    icon: RiComputerLine,
+  },
+  {
+    title: 'Title Something 2',
+    about: 'About Something 2',
+    icon: RiComputerLine,
+  },
+  {
+    title: 'Title <b>Something</b> 3',
+    about: 'About Something 3',
+    icon: RiComputerLine,
+  },
+  {
+    title: 'Title Something 4',
+    about: 'About Something 4',
+    icon: RiComputerLine,
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
-const index = () => {
+import { GetServerSidePropsContext, GetStaticProps, GetStaticPropsContext } from 'next';
+
+const index = ({ services }) => {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
+    <h1 className='text-3xl font-bold underline'>
+      Hello world!
+    </h1>
+  );
 };
 
 export default index;
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+
+  const res = await fetch('http://localhost:3000/api/services');
+  const data = await res.json();
+
+  return {
+    props: {
+      services: data.services,
+    },
+  };
+};
