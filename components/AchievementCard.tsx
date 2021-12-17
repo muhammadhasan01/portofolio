141b2b385aab2e73b7bdf6d73df189354b775f26
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-18 05:20:04 +0700

    chore: fix typo

--- components/AchievementCard.tsx
+++ components/AchievementCard.tsx
@@ -15,10 +15,10 @@ const AchievementCard: FunctionComponent<{ achievement: IAchievement }> = (
     <div>
       <div className='my-1 grid grid-cols-12 flex flex-col items-center justify-left'>
         <Icon className='col-span-2' size={25}/>
-        <span dangerouslySetInnerHTML={createMarkup()} className='font-bold text-medium -ml-2 lg:-ml-7 col-span-10' />
+        <span dangerouslySetInnerHTML={createMarkup()} className='font-bold text-medium -ml-1 lg:-ml-7 col-span-10' />
       </div>
       <div className='md:text-lg flex justify-left items-center'>
-        <span className='font-semibold text-base md:text-medium'>Issued By {issuedBy}</span>
+        <span className='font-semibold text-base md:text-medium'>Issued by {issuedBy}</span>
       </div>
       <p className='my-2 font-medium text-base text-gray-800 dark:text-white lg:text-medium'>
         {description}
